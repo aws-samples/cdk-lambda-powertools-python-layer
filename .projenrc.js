cf303a84c5cabc8d6daba35d8d3fbadec294dcf7
Alexander Melnyk <am29d@hey.com>
2022-01-10 15:11:13 +0100

    add org

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,13 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   authorName: 'Alexander Melnyk',
+  authorOrganization: true,
   keywords: ['aws', 'cdk', 'powertools', 'python', 'layer', 'lambda', 'devax'],
   cdkVersion: '2.2.0',
   defaultReleaseBranch: 'main',
-  majorVersion: '0',
+  majorVersion: '2',
   name: 'cdk-lambda-powertools-python-layer',
-  repositoryUrl: 'https://github.com/amelnyk/cdk-lambda-powertools-python-layer.git',
+  repositoryUrl: 'https://github.com/aws-samples/cdk-lambda-powertools-python-layer.git',
   description: 'A lambda layer for AWS Powertools for python',
   github: false,
   publishToPypi: {
@@ -17,7 +18,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     twitter: 'am29d',
     announce: false,
   },
-  licensed: false,
+  license: 'MIT-0',
 });
 
 
