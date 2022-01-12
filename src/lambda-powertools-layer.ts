b699805a1964b4c9cb23422ee91a42446f44011c
Alexander Melnyk <amelnyk@amazon.com>
2022-01-12 10:58:44 +0100

    docs: simplified examples

--- src/lambda-powertools-layer.ts
+++ src/lambda-powertools-layer.ts
@@ -3,15 +3,15 @@ import { aws_lambda as lambda } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 /**
- * Properties necessary to create Powertools layer for python.
+ * Properties for Powertools layer for python.
  */
 export interface PowertoolsLayerProps {
   /**
-   * The powertools package version form pypi repository.
+   * The powertools package version from pypi repository.
    */
   readonly version?: string;
   /**
-   * A flag to decide wether to include the extras package, used for parsing.
+   * A flag for the pydantic extras dependency, used for parsing.
    * This will increase the size of the layer significantly. If you don't use parsing, ignore it.
    */
   readonly includeExtras?: boolean;
@@ -25,9 +25,9 @@ export class LambdaPowertoolsLayer extends lambda.LayerVersion {
 
   /**
    * creates build argument for the Dockerfile.
-   * We have multiple combinations between version and extras package that results in different suffix for the installation.
+   * There are multiple combinations between version and extras package that results in different suffix for the installation.
    * With and without version, with and without extras flag.
-   * We construct one suffix here because it is easier to do than inside the Dockerfile with bash commands.
+   * We construct one suffix here because it is easier to do in code than inside the Dockerfile with bash commands.
    * For example, if we set extras=true and version=1.22.0 we get '[pydantic]==1.22.0'.
    *
    */
